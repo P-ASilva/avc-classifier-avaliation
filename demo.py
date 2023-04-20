1084b50230fda742a3d34c22c141cb4fbc9244bb
Pedro Antônio Silva <105285216+P-ASilva@users.noreply.github.com>
2023-04-20 17:06:36 -0300

    base

--- demo.py
+++
@@ -1,8 +0,0 @@
-from sklearn.model_selection import train_test_split
-import pandas as pd
-from sklearn.tree import plot_tree
-import matplotlib.pyplot as plt
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.5)from sklearn.tree import DecisionTreeClassifier
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
